1cfb24015d7a8713b74a14aa1fa77151eb5878af
va1eria <valeria.varadi@outlook.com>
2022-02-15 20:11:11 +0100

    Foreign keys adatbazisban

---
+++ AllatmenhelyiNyilvantarto/SQL_ForeignKeys.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Orokbefogadas] ADD CONSTRAINT [Orokbefogadas_Allat_FK] FOREIGN KEY ([AllatID]) REFERENCES [Allat]([Nev]);
+ALTER TABLE [Orokbefogadas] ADD CONSTRAINT [Orokbefogadas_Orokbefogado_FK] FOREIGN KEY ([OrokbefogadoID]) REFERENCES [Orokbefogado]([Id]);
+
+ALTER TABLE [Allat] ADD [GondozoID] INT NULL;
+ALTER TABLE [Allat] ADD CONSTRAINT [Allat_Gondozo_FK] FOREIGN KEY ([GondozoID]) REFERENCES [Gondozo]([Id]);
